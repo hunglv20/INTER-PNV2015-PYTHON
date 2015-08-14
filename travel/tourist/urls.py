13739aebbccd72278970da3f2613bff822bdcd90
hungluu20 <hunglv.qng@gmail.com>
2015-08-14 18:06:05 +0700

    update project travel

--- travel/tourist/urls.py
+++ travel/tourist/urls.py
@@ -4,6 +4,7 @@ from tourist import views
 
 urlpatterns = patterns('',
     url(r'^index/', views.IndexView.as_view(), name='index'),
+    # url(r'^delete/', views.DeleteIndexView.as_view(), name='delete'),
     url(r'^(?P<pk>\d+)/$', views.FoodView.as_view(), name='food'),
     url(r'^(?P<pk>\d+)/hotel/$', views.HotelView.as_view(), name='hotel'),
     url(r'^$',views.IndexViewD.as_view(), name='home'),
@@ -14,4 +15,11 @@ urlpatterns = patterns('',
     url(r'^detail/$', views.DetailViewD.as_view(), name='detail'),
     # url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     # url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+
+    url(r'^new/$', views.tourist_create, name='tourist_new'),
+    url(r'^edit/(?P<pk>\d+)$', views.tourist_update, name='tourist_edit'),
+    url(r'^delete/(?P<pk>\d+)$', views.tourist_delete, name='tourist_delete'),
+
+    url(r'^mefood/$', views.FoodView.as_view(), name='mefood'),
+    url(r'^mehotel/$', views.HotelView.as_view(), name='mehotel'),
 )
--- travel/tourist/views.py
+++ travel/tourist/views.py
@@ -7,12 +7,6 @@ from tourist.models import Tourist
 from tourist.models import Food
 from tourist.models import Hotel
 
-class IndexView(generic.ListView):
-    template_name = 'tourist/index.html'
-    context_object_name = 'latest_tourist_list'
-
-    def get_queryset(self):
-        return Tourist.objects.all()
 
 class FoodView(generic.ListView):
     template_name = 'tourist/food.html'
@@ -20,7 +14,19 @@ class FoodView(generic.ListView):
     def get_queryset(self):
         return Food.objects.all()
 
+class MeFoodView(generic.ListView):
+    template_name = 'tourist/food.html'
+    context_object_name = 'latest_food_list'
+    def get_queryset(self):
+        return Food.objects.all()
+
 class HotelView(generic.ListView):
+    template_name = 'tourist/hotel.html'
+    context_object_name = 'latest_hotel_list'
+    def get_queryset(self):
+        return Hotel.objects.all()
+
+class MeHotelView(generic.ListView):
     template_name = 'tourist/hotel.html'
     context_object_name = 'latest_hotel_list'
     def get_queryset(self):
@@ -69,5 +75,52 @@ class DetailViewD(generic.ListView):
     def get_queryset(self):
         return Tourist.objects.order_by('-id')[:3]
 
+########################################################
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
+
+from tourist.models import Tourist
+
+
+
+class TouristForm(ModelForm):
+    class Meta:
+        model = Tourist
+        fields = ['name', 'address', 'price', 'means', 'detail', 'image', 'hotel', 'food']
+
+class IndexView(generic.ListView):
+    template_name = 'tourist/index.html'
+    context_object_name = 'latest_tourist_list'
+
+    def get_queryset(self):
+        return Tourist.objects.all()
+
+# def tourist_list(request, template_name='tourist/index.html'):
+#     tourist = Tourist.objects.all()
+#     data = {}
+#     data['object_list'] = tourists
+#     return render(request, template_name, data)
+
+def tourist_create(request, template_name='tourist/index_form.html'):
+    form = TouristForm(request.POST, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/tourist/index')
+    return render(request, template_name, {'form':form})
+
+def tourist_update(request, pk, template_name='tourist/index_form.html'):
+    tourist = get_object_or_404(Tourist, pk=pk)
+    form = TouristForm(request.POST, request.FILES, instance=tourist or None)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/tourist/index')
+    return render(request, template_name, {'form':form})
+
+def tourist_delete(request, pk, template_name='tourist/tourist_confirm_delete.html'):
+    tourist = get_object_or_404(Tourist, pk=pk)    
+    if request.method=='POST':
+        tourist.delete()
+        return HttpResponseRedirect('/tourist/index')
+    return render(request, template_name, {'object':tourist})
 
